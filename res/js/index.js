d2a83e95d19f96cba27c5f311ce5adac7cb900f7
A.Loo <loo_allan@yahoo.com>
2020-10-19 14:58:31 +0300

    Made the code into jQuery.

--- res/js/index.js
+++ res/js/index.js
@@ -1,24 +1,14 @@
+$("#avatarIco").click(function () {
+    $("#profileDropdown").toggle()
+});
 
-function dropDownClicked() {
-    document.getElementById("profileDropdown").classList.toggle("show");
-}
-//Got help from here.
-//https://www.w3schools.com/howto/howto_js_dropdown.asp
-
-window.onclick = function(event) {
-    if (!event.target.matches('.avatar')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
+$("body").click(function(event ){
+    var $target = $(event.target);
+    if(!$target.parents().is(".dropdown-content") && !$target.is(".dropdown-content") && !$target.is(".avatar")){
+        $("body").find(".dropdown-content").hide();
     }
-}
-
-var request = new XMLHttpRequest();
+});
+let request = new XMLHttpRequest();
 
 request.responseType = 'json';
 request.open('GET', 'https://private-anon-2f317f3893-wad20postit.apiary-mock.com/users/1');
@@ -26,7 +16,7 @@ request.send();
 
 request.onload = function () {
     let user = request.response;
-    document.getElementById("nameUser").innerText = user.firstname + " " + user.lastname;
-    document.getElementById("emailUser").innerText = user.email;
-    document.getElementById("avatarIco").src = user.avatar;
-}
+    $("#nameUser").text(user.firstname + " " + user.lastname);
+    $("#emailUser").text(user.firstname + " " + user.email);
+    $("#avatarIco").attr("src", user.avatar);
+};
