b2b80f4a9468d0d56879046247a0e27416dd4821
taavopp <44435235+taavopp@users.noreply.github.com>
2020-10-22 22:23:16 +0300

    Task 3

--- res/js/index.js
+++ res/js/index.js
@@ -8,15 +8,54 @@ $("body").click(function(event ){
         $("body").find(".dropdown-content").hide();
     }
 });
-let request = new XMLHttpRequest();
 
-request.responseType = 'json';
-request.open('GET', 'https://private-anon-2f317f3893-wad20postit.apiary-mock.com/users/1');
-request.send();
+let userRequest = new XMLHttpRequest()
+let browseRequest = new XMLHttpRequest();
 
-request.onload = function () {
-    let user = request.response;
+userRequest.responseType = 'json';
+userRequest.open('GET', 'https://private-anon-2f317f3893-wad20postit.apiary-mock.com/users/1');
+userRequest.send();
+
+browseRequest.responseType = 'json';
+browseRequest.open('GET', 'https://private-anon-4dfe848681-wad20postit.apiary-mock.com/profiles');
+browseRequest.send();
+
+browseRequest.onload = function () {
+    let data = browseRequest.response;
+    createItems(data);
+};
+
+userRequest.onload = function () {
+    let user = userRequest.response;
     $("#nameUser").text(user.firstname + " " + user.lastname);
     $("#emailUser").text(user.firstname + " " + user.email);
     $("#avatarIco").attr("src", user.avatar);
 };
+
+const createItems = function (data) {
+    const users = $('.browse-users');
+
+    data.forEach(function(index, elem){
+        const element = data[elem];
+        const user = $('<div class="browse-user"></div>');
+        const avatar = $(`<div class="browse-image-container"><img src="${element.avatar}"></div>`);
+        const name = $(`<p class="browse-user-text">${element.firstname} ${element.lastname}</p>`);
+        const button = $('<button class="browse-follow-button">Follow</button>');
+        users.prepend(user);
+        user.prepend(avatar);
+        user.append(name);
+        user.append(button);
+    });
+
+    $('.browse-follow-button').click(function(event){
+        const button = $(event.currentTarget);
+
+        if (button.hasClass('was-pressed')) {
+            button.removeClass('was-pressed');
+            button.text('Follow');
+        } else {
+            button.addClass('was-pressed');
+            button.text('Followed');
+        }
+    });
+}
