1e5521d167a705d2c0b7492944e1e4fed02c6a92
Karl Rapur <rapurkarl@gmail.com>
2020-10-23 20:48:10 +0300

    Implemented task 2.

--- res/js/index.js
+++ res/js/index.js
@@ -1,10 +1,10 @@
-$("#avatarIco").click(function () {
+$("#avatarIco").click(function() {
     $("#profileDropdown").toggle()
 });
 
-$("body").click(function(event ){
+$("body").click(function(event) {
     var $target = $(event.target);
-    if(!$target.parents().is(".dropdown-content") && !$target.is(".dropdown-content") && !$target.is(".avatar")){
+    if (!$target.parents().is(".dropdown-content") && !$target.is(".dropdown-content") && !$target.is(".avatar")) {
         $("body").find(".dropdown-content").hide();
     }
 });
@@ -20,23 +20,22 @@ browseRequest.responseType = 'json';
 browseRequest.open('GET', 'https://private-anon-4dfe848681-wad20postit.apiary-mock.com/profiles');
 browseRequest.send();
 
-browseRequest.onload = function () {
+browseRequest.onload = function() {
     let data = browseRequest.response;
     createItems(data);
 };
 
-userRequest.onload = function () {
+userRequest.onload = function() {
     let user = userRequest.response;
     $("#nameUser").text(user.firstname + " " + user.lastname);
     $("#emailUser").text(user.firstname + " " + user.email);
     $("#avatarIco").attr("src", user.avatar);
 };
 
-const createItems = function (data) {
+const createItems = function(data) {
     const users = $('.browse-users');
 
-    data.forEach(function(index, elem){
-        const element = data[elem];
+    for (element of data) {
         const user = $('<div class="browse-user"></div>');
         const avatar = $(`<div class="browse-image-container"><img src="${element.avatar}"></div>`);
         const name = $(`<p class="browse-user-text">${element.firstname} ${element.lastname}</p>`);
@@ -45,9 +44,9 @@ const createItems = function (data) {
         user.prepend(avatar);
         user.append(name);
         user.append(button);
-    });
+    };
 
-    $('.browse-follow-button').click(function(event){
+    $('.browse-follow-button').click(function(event) {
         const button = $(event.currentTarget);
 
         if (button.hasClass('was-pressed')) {
@@ -58,4 +57,65 @@ const createItems = function (data) {
             button.text('Followed');
         }
     });
+}
+
+
+//Task 2
+let postsRequest = new XMLHttpRequest();
+
+postsRequest.responseType = 'json';
+postsRequest.open('GET', 'https://private-anon-a878d01bcd-wad20postit.apiary-mock.com/posts');
+postsRequest.send();
+
+postsRequest.onload = function() {
+    let data = postsRequest.response;
+    createPosts(data);
+};
+
+
+const createPosts = function(data) {
+    const mainFeed = $('.main-container');
+
+    for (post of data) {
+        const postOwner = post.author;
+        const postMedia = post.media;
+
+        const postDiv = $('<div class="post"></div>');
+        const postAuthor = $(`<div class="post-author"><span class="post-author-info">` +
+            `<img src="${postOwner.avatar}" alt="Post author"><small>${postOwner.firstname} ` +
+            `${postOwner.lastname}</small></span><small>${post.createTime}</small></div>`);
+        postDiv.append(postAuthor);
+
+
+        if (postMedia !== null) {
+            let postView = null;
+            if (postMedia.type === "image") {
+                postView = $(`<div class="post-image"><img src="${postMedia.url}" alt=""></div>`);
+            } else postView = $(`<div class="post-image"><video width="320" height="240" controls>` +
+                `<source src="${postMedia.url}" type="video/mp4">` +
+                `Your browser does not support the video tag.</video></div>`);
+            postDiv.append(postView);
+        }
+
+        if (post.text !== null) {
+            const postTitle = $(`<div class="post-title"><h3>${post.text}</h3></div>`);
+            postDiv.append(postTitle);
+        }
+
+        const postLikes = $(`<div class="post-actions"><button type="button" name="like" class="like-button">${post.likes}</button></div>`)
+        postDiv.append(postLikes);
+
+
+        $('.like-button').click(function(event) {
+            const button = $(event.currentTarget);
+
+            if (button.hasClass('liked')) {
+                button.removeClass('liked');
+            } else button.addClass('liked');
+        });
+
+        mainFeed.append(postDiv);
+    }
+
+
 }
